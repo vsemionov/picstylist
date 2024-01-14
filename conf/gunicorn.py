4f2799938a2a1e3a82df0cab32d5f23e89d1d04e
Victor Semionov <vsemionov@gmail.com>
2024-01-15 01:19:10 +0200

    Revert "access log"
    This reverts commit f94890f8a6b28c2b84f2c6b84ecc77f17fea17b4.

--- conf/gunicorn.py
+++ conf/gunicorn.py
@@ -4,6 +4,3 @@ bind = "0.0.0.0:8000"
 workers = os.cpu_count()
 worker_class = "gthread"
 threads = 10
-
-accesslog = "-"
-access_log_format = '%(h)s - %(u)s [%(t)s] "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({x-forwarded-for}i)s" %({x-request-id}i)s'

57def7abe779547c10e31aea61811227ffa98480
Victor Semionov <vsemionov@gmail.com>
2024-01-17 05:38:10 +0200

    test

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ def before_request():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    app.logger.info('Test ') ###
+    app.logger.info(request.headers) ###
+    import time.time ###
+    time.sleep(2) ###
     if request.method == 'POST':
         content_file = request.files['content']
         style_file = request.files['style']

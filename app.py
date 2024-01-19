2c3df1039ffb8ff65f69520b58dc71f8c8fe1bfa
Victor Semionov <vsemionov@gmail.com>
2024-01-19 18:15:22 +0200

    removing captcha because there is no obvious way to remember the expected text without requiring a login, and it wound annoy legitimate users

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask, request, url_for, abort, redirect, render_template, mak
     session, jsonify
 from werkzeug.utils import secure_filename
 from jinja2 import TemplateNotFound
-from flask_limiter import RateLimitExceeded
 
 from web import settings
 
@@ -16,13 +15,6 @@ app, limiter = settings.configure(app)
 @app.route('/', methods=['GET', 'POST'])
 @limiter.limit('5/minute;50/hour;200/day', methods=['POST'])
 def index():
-    captcha_limit = limiter.shared_limit('3/hour', scope='captcha', cost=lambda: int(request.method == 'POST'))
-    try:
-        with captcha_limit:
-            use_captcha = False if request.method == 'POST' else limiter.current_limit.remaining <= 0  # TODO: untested
-    except RateLimitExceeded:
-        use_captcha = True
-
     if request.method == 'POST':
         # TODO: validate form and secure CSRF
         file = request.files['file']

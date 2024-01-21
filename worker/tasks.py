3952fc1a1bc1c8d439ef602d71ca225d6e4ad52b
Victor Semionov <vsemionov@gmail.com>
2024-01-21 05:42:28 +0200

    wip

--- worker/tasks.py
+++ worker/tasks.py
@@ -1,10 +1,14 @@
 import os
+import logging
 
-import worker.model
+
+logger = logging.getLogger(__name__)
 
 
 def style_image(content_path, style_path, result_stem):
     try:
+        import worker.model
+        logger.info(f'Processing')
         return worker.model.fast_style_transfer(content_path, style_path, result_stem)
     finally:
         for path in [content_path, style_path]:

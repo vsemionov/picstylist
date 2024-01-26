71b95ae3017dec706decdedcee042a7039584809
Victor Semionov <vsemionov@gmail.com>
2024-01-26 13:38:42 +0200

    revert to 512, output quality is better

--- worker/model.py
+++ worker/model.py
@@ -6,17 +6,20 @@ import tensorflow as tf
 import tensorflow_hub as hub
 from PIL import Image
 
+
+MAX_SIZE = 512
+
+
 os.environ['TFHUB_CACHE_DIR'] = str(Path(__file__).parent.parent / 'model')
 hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
 
 
 def load_image(image_path):
-    max_size = int(os.environ['MAX_IMAGE_SIZE'])
     image = Image.open(image_path).convert('RGB')
     width, height = image.size
     long_edge = max(width, height)
-    if long_edge > max_size:
-        scale = max_size / long_edge
+    if long_edge > MAX_SIZE:
+        scale = MAX_SIZE / long_edge
         width, height = max(int(width * scale), 1), max(int(height * scale), 1)
         image = image.resize((width, height), resample=Image.Resampling.BICUBIC)
     return tf.constant(image, dtype=tf.float32)[tf.newaxis, :] / 255

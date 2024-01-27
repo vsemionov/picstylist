3fc9bf693962166689f58f39120a12eb6439d17a
Victor Semionov <vsemionov@gmail.com>
2024-01-27 13:50:25 +0200

    set sentry server name

--- common/integration.py
+++ common/integration.py
@@ -10,6 +10,7 @@ from common import VERSION
 def configure_sentry(with_flask=False):
     app_env = os.environ['APP_ENV']
     sentry_dsn = os.environ['SENTRY_DSN']
+    service_name = os.environ['SERVICE_NAME']
     if not sentry_dsn:
         if app_env != 'development':
             raise ValueError('SENTRY_DSN is required on remote environments.')
@@ -19,4 +20,5 @@ def configure_sentry(with_flask=False):
     if with_flask:
         from sentry_sdk.integrations.flask import FlaskIntegration
         integrations = [FlaskIntegration()] + integrations
-    sentry_sdk.init(sentry_dsn, release=VERSION, environment=app_env, integrations=integrations)
+    sentry_sdk.init(sentry_dsn, release=VERSION, environment=app_env, server_name=service_name,
+        integrations=integrations)

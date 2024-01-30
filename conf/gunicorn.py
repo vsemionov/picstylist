5991bc0e9e1ffc350f01766b38b0470a38ff10cf
Victor Semionov <vsemionov@gmail.com>
2024-01-30 14:54:06 +0200

    keepalive connections from nginx to gunicorn

--- conf/gunicorn.py
+++ conf/gunicorn.py
@@ -6,6 +6,8 @@ bind = '0.0.0.0:8000'
 workers = os.cpu_count()
 worker_class = 'gevent'
 worker_connections = 1024
+keepalive = 120
+timeout = 30
 
 accesslog = '-'
 access_log_format = '[%({X-Request-ID}i)s] %(h)s - "%(r)s" %(L)s %(s)s %(b)s "%(f)s" "%(a)s"'

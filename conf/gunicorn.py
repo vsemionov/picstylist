a9c1e1e0afa2b9c003072935603d85843f7f64e5
Victor Semionov <vsemionov@gmail.com>
2024-01-30 14:47:40 +0200

    use gevent and patch sqlite

--- conf/gunicorn.py
+++ conf/gunicorn.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 
 bind = '0.0.0.0:8000'
 workers = os.cpu_count()
@@ -7,3 +9,11 @@ worker_connections = 1024
 
 accesslog = '-'
 access_log_format = '[%({X-Request-ID}i)s] %(h)s - "%(r)s" %(L)s %(s)s %(b)s "%(f)s" "%(a)s"'
+
+
+def post_worker_init(worker):
+    import sqlite3
+    import gsqlite3
+    gsqlite3.too_slow = -1  # disable sync execution
+    sys.modules[sqlite3.__name__] = gsqlite3
+    worker.log.info('Gevent initialized.')

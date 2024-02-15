d9ffb7e3092ef7a2beb4b0434afa848628e8d388
Victor Semionov <vsemionov@gmail.com>
2024-02-15 11:13:17 +0200

    refactoring

--- worker/models.py
+++ worker/models.py
@@ -9,15 +9,15 @@ from PIL import Image
 from worker import iterative
 
 
-MAX_SIZE = 512
-STEPS = 500
+MAX_SIZE = 128
+STEPS = 100
 
 
 os.environ['TFHUB_CACHE_DIR'] = str(Path(__file__).parent.parent / 'models')
 fast_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
 
 
-def load_image(image_path):
+def _load_image(image_path):
     image = Image.open(image_path).convert('RGB')
     width, height = image.size
     long_edge = max(width, height)
@@ -25,7 +25,11 @@ def load_image(image_path):
         scale = MAX_SIZE / long_edge
         width, height = max(int(width * scale), 1), max(int(height * scale), 1)
         image = image.resize((width, height), resample=Image.Resampling.BICUBIC)
-    return tf.constant(image, dtype=tf.float32)[tf.newaxis, :] / 255
+    return image
+
+
+def load_image(image_path):
+    return tf.constant(_load_image(image_path), dtype=tf.float32)[tf.newaxis, :] / 255
 
 
 def to_image(tensor):
@@ -55,10 +59,7 @@ def fast_style_transfer(base_path, content_filename, style_filename, strength, r
 
 
 def iterative_style_transfer(base_path, content_filename, style_filename, strength, result_filename):
-    content_image = iterative.image_loader(base_path / content_filename)
-    style_image = iterative.image_loader(base_path / style_filename)
-
-    output = iterative.run_style_transfer(iterative.cnn, iterative.cnn_normalization_mean,
-        iterative.cnn_normalization_std, content_image, style_image, content_image.clone(), int(STEPS * strength / 100))
-
-    return save_image(iterative.unloader(output[0]), base_path / result_filename)
+    content_image = _load_image(base_path / content_filename)
+    style_image = _load_image(base_path / style_filename)
+    output = iterative.run_style_transfer(content_image, style_image, int(STEPS * strength / 100))
+    return save_image(output, base_path / result_filename)

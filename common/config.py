690df79cbd649a55ff5b4af08722784a2a1b1d12
Victor Semionov <vsemionov@gmail.com>
2024-02-16 03:10:37 +0200

    removed unused settings

--- common/config.py
+++ common/config.py
@@ -7,9 +7,6 @@ SYSTEM_QUEUE = 'system'
 HEALTH_CHECK_INTERVAL = 15 * 60
 HEALTH_CHECK_VALIDITY = 30 * 60
 HEALTH_CHECK_JOB_ID = 'health_check'
-
-IMAGE_CHECK_INTERVAL = 15 * 60
-IMAGE_CHECK_VALIDITY = 30 * 60
 IMAGE_CHECK_JOB_ID = 'image_check'
 
 LOG_FORMAT = '%(levelname)s in %(name)s: %(message)s'

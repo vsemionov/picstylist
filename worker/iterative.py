5605c9932b6155bc4a1cf8be51ab27e019e0570a
Victor Semionov <vsemionov@gmail.com>
2024-05-12 06:15:25 +0300

    improvements

--- worker/iterative.py
+++ worker/iterative.py
@@ -9,9 +9,11 @@ import torchvision.transforms as transforms
 from torchvision.models import vgg19, VGG19_Weights
 
 
-NUM_STEPS = 300
-CONTENT_LAYERS = ['conv_4']
-STYLE_LAYERS = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+NUM_STEPS = 320
+LEARNING_RATE = 1.0
+LBFGS_KWARGS = {'history_size': 10}
+CONTENT_LAYERS = ['conv4_2']
+STYLE_LAYERS = ['conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1']
 MAX_STYLE_WEIGHT = 1_000_000
 CONTENT_WEIGHT = 1
 
@@ -21,42 +23,39 @@ logger = logging.getLogger(__name__)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 torch.set_default_device(device)
 
-cnn = vgg19(weights=VGG19_Weights.DEFAULT).features.eval()
+cnn = vgg19(weights=VGG19_Weights.DEFAULT).features.eval().requires_grad_(False)
 cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406])
 cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225])
 
 to_tensor = transforms.ToTensor()
 to_image = transforms.ToPILImage()
-convert_image = lambda image: to_tensor(image).unsqueeze(0).to(device, torch.float)
+convert_image = lambda image: to_tensor(image).to(device)
 
 
 def gram_matrix(input):
     a, b, c, d = input.size()
-    features = input.view(a * b, c * d)
-    G = torch.mm(features, features.t())
-    return G.div(a * b * c * d)
+    features = input.view(b, c * d)
+    G = features @ features.t()
+    return G / (b * c * d)
 
 
 class ContentLoss(nn.Module):
     def __init__(self, target):
         super().__init__()
-        # we 'detach' the target content from the tree used
-        # to dynamically compute the gradient: this is a stated value,
-        # not a variable. Otherwise the forward method of the criterion
-        # will throw an error.
-        self.target = target.detach()
+        self.target = target
 
     def forward(self, input):
-        self.loss = torch.tensor(0)
-        if input.shape == self.target.shape:
+        if input.size() == self.target.size():
             self.loss = F.mse_loss(input, self.target)
+        else:
+            self.loss = 0
         return input
 
 
 class StyleLoss(nn.Module):
     def __init__(self, target_feature):
         super(StyleLoss, self).__init__()
-        self.target = gram_matrix(target_feature).detach()
+        self.target = gram_matrix(target_feature)
 
     def forward(self, input):
         G = gram_matrix(input)
@@ -67,9 +66,6 @@ class StyleLoss(nn.Module):
 class Normalization(nn.Module):
     def __init__(self, mean, std):
         super(Normalization, self).__init__()
-        # .view the mean and std to make them [C x 1 x 1] so that they can
-        # directly work with image Tensor of shape [B x C x H x W].
-        # B is batch size. C is number of channels. H is height and W is width.
         self.mean = mean.view(-1, 1, 1)
         self.std = std.view(-1, 1, 1)
 
@@ -85,43 +81,44 @@ def get_style_model_and_losses(content_image, style_image):
 
     model = nn.Sequential(normalization)
 
-    i = 0
+    block = 1
+    conv = 0
     for layer in cnn.children():
         if isinstance(layer, nn.Conv2d):
-            i += 1
-            name = f'conv_{i}'
+            conv += 1
+            name = f'conv{block}_{conv}'
+        elif isinstance(layer, nn.BatchNorm2d):
+            name = f'bn{block}_{conv}'
         elif isinstance(layer, nn.ReLU):
-            name = f'relu_{i}'
-            # The in-place version doesn't play very nicely with the ``ContentLoss``
-            # and ``StyleLoss`` we insert below. So we replace with out-of-place
-            # ones here.
+            name = f'relu{block}_{conv}'
             layer = nn.ReLU(inplace=False)
         elif isinstance(layer, nn.MaxPool2d):
-            name = f'pool_{i}'
-        elif isinstance(layer, nn.BatchNorm2d):
-            name = f'bn_{i}'
+            name = f'pool{block}'
+            layer = nn.AvgPool2d(2)
+            block += 1
+            conv = 0
         else:
             raise RuntimeError(f'Unrecognized layer: {layer.__class__.__name__}')
 
         model.add_module(name, layer)
 
         if name in CONTENT_LAYERS:
-            target = model(content_image).detach()
+            target = model(content_image)
             content_loss = ContentLoss(target)
-            model.add_module(f'content_loss_{i}', content_loss)
+            model.add_module(f'content_loss_{block}_{conv}', content_loss)
             content_losses.append(content_loss)
 
         if name in STYLE_LAYERS:
-            target_feature = model(style_image).detach()
+            target_feature = model(style_image)
             style_loss = StyleLoss(target_feature)
-            model.add_module(f'style_loss_{i}', style_loss)
+            model.add_module(f'style_loss_{block}_{conv}', style_loss)
             style_losses.append(style_loss)
 
-    for i in range(len(model) - 1, -1, -1):
-        if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
-            break
-
-    model = model[:(i + 1)]
+    last = -1
+    for i, layer in enumerate(model):
+        if isinstance(layer, (ContentLoss, StyleLoss)):
+            last = i
+    model = model[:last + 1]
 
     return model, content_losses, style_losses
 
@@ -129,46 +126,45 @@ def get_style_model_and_losses(content_image, style_image):
 def run_style_transfer(content_image, style_image, content_weight, style_weight):
     model, content_losses, style_losses = get_style_model_and_losses(content_image, style_image)
 
-    work_image = content_image
+    work_image = torch.rand(content_image.size())
     work_image.requires_grad_(True)
 
     model.eval()
-    model.requires_grad_(False)
 
-    optimizer = optim.LBFGS([work_image], history_size=10)
+    optimizer = optim.LBFGS([work_image], lr=LEARNING_RATE, max_iter=NUM_STEPS, **LBFGS_KWARGS)
 
     step = 0
-    while step <= NUM_STEPS:
-        def closure():
-            nonlocal step
 
-            with torch.no_grad():
-                work_image.clamp_(0, 1)
+    def get_loss_and_grad():
+        nonlocal step
 
-            optimizer.zero_grad()
-            model(work_image)
-            content_loss = 0
-            style_loss = 0
+        with torch.no_grad():
+            work_image.clamp_(0, 1)
 
-            for cl in content_losses:
-                content_loss += cl.loss
-            for sl in style_losses:
-                style_loss += sl.loss
+        model(work_image)
 
-            content_loss *= content_weight
-            style_loss *= style_weight
+        content_loss = 0
+        style_loss = 0
+        for cl in content_losses:
+            content_loss = content_loss + cl.loss
+        for sl in style_losses:
+            style_loss = style_loss + sl.loss
+        content_loss = content_loss * content_weight
+        style_loss = style_loss * style_weight
+        loss = content_loss + style_loss
 
-            loss = content_loss + style_loss
-            loss.backward()
+        optimizer.zero_grad()
+        loss.backward()
 
-            step += 1
-            if step % 50 == 0:
-                logger.info('Step %d/%d:', step, NUM_STEPS)
-                logger.info('Content loss: %.2f, style loss: %.2f', content_loss.item(), style_loss.item())
+        step += 1
 
-            return loss
+        if step % 50 == 0 or step in (1, NUM_STEPS):
+            logger.info('Step: %d/%d, content loss: %.2e, style loss: %.2e', step, NUM_STEPS, content_loss.item(),
+                style_loss.item())
 
-        optimizer.step(closure)
+        return loss
+
+    optimizer.step(get_loss_and_grad)
 
     with torch.no_grad():
         work_image.clamp_(0, 1)
@@ -177,13 +173,14 @@ def run_style_transfer(content_image, style_image, content_weight, style_weight)
 
 
 def style_transfer(content_image, style_image, strength):
-    content_image = convert_image(content_image)
-    style_image = convert_image(style_image)
+    content_image = convert_image(content_image).unsqueeze(0)
+    style_image = convert_image(style_image).unsqueeze(0)
     style_weight = MAX_STYLE_WEIGHT * strength / 100
     output = run_style_transfer(content_image, style_image, CONTENT_WEIGHT, style_weight)
     return to_image(output[0])
 
 
+# TODO
 n = 0
 last_layer = None
 for i in range(len(cnn)):
